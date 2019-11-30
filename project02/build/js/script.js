3b989679d0f8b0d2686e6a1837d84044f1a5f891
andresns <andresm.navarro@gmail.com>
2019-11-30 15:06:35 -0800

    refactor header

--- project02/build/js/script.js
+++ project02/build/js/script.js
@@ -1,13 +1,24 @@
 (function () {
     let toggleButton = document.getElementById("toggle-menu");
+    let headerContainer = document.getElementsByClassName("header")[0];
     let headerNav = document.getElementsByClassName("header__nav")[0];
-    let height = headerNav.scrollHeight;
+    let headerNavHeight = headerNav.scrollHeight;
+    
+    
+
     toggleButton.addEventListener("click", function (event) {
         event.preventDefault();
+        
+        if(!headerNav.classList.contains("is-open")){
+            let headerContainerHeight = headerContainer.scrollHeight;
+        headerNav.style.setProperty("top", headerContainerHeight + "px");
+        }
+        
         let menu = document.getElementById("main-menu");
         menu.classList.toggle("is-open");
+
         if(headerNav.classList.contains("is-open")){
-            headerNav.style.setProperty("height", height + "px");
+            headerNav.style.setProperty("height", headerNavHeight + 3 + "px");
         }else {
             headerNav.style.setProperty("height", 0);
         }
