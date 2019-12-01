5f20023292157ca2f99663618118e0611409ebf8
andresns <andresm.navarro@gmail.com>
2019-11-30 19:58:39 -0800

    add footer for md and lg

--- project02/build/js/script.js
+++ project02/build/js/script.js
@@ -3,9 +3,9 @@
     let headerContainer = document.getElementsByClassName("header")[0];
     let headerNav = document.getElementsByClassName("header__nav")[0];
     let headerNavHeight = headerNav.scrollHeight;
-    
-    
+    let menu = document.getElementById("main-menu");
 
+    //functions for responsive menu
     toggleButton.addEventListener("click", function (event) {
         event.preventDefault();
         
@@ -13,8 +13,7 @@
             let headerContainerHeight = headerContainer.scrollHeight;
         headerNav.style.setProperty("top", headerContainerHeight + "px");
         }
-        
-        let menu = document.getElementById("main-menu");
+
         menu.classList.toggle("is-open");
 
         if(headerNav.classList.contains("is-open")){
