e7c780d1e20cc5891e730a131a88775c04f77655
andresns <andresm.navarro@gmail.com>
2019-12-03 10:26:21 -0800

    add toggleMenu function

--- project02/build/js/script.js
+++ project02/build/js/script.js
@@ -2,25 +2,45 @@
     let toggleButton = document.getElementById("toggle-menu");
     let headerContainer = document.getElementsByClassName("header")[0];
     let headerNav = document.getElementsByClassName("header__nav")[0];
-    let headerNavHeight = headerNav.scrollHeight;
-    
+    let menu = document.getElementById("main-menu");
+    // let headerNavHeight = headerNav.scrollHeight;
+
+    // window.onresize = function(event) {
+    //     let headerNavHeight = headerNav.scrollHeight;
+    //     if(window.innerWidth>=1024){
+    //         if(headerNav.classList.contains("is-open")){
+    //             headerNav.style.setProperty("height", "auto");
+    //             headerNav.classList.remove("is-open");
+    //         }
+    //     }else{
+    //         headerNav.classList.remove("is-open");
+    //         headerNav.style.setProperty("height", 0);
+    //         // headerContainerHeight.style.setProperty("height", 0);
+    //         // let headerContainerHeight = headerContainer.scrollHeight;
+    //         // headerNav.style.setProperty("top", headerContainerHeight + "px");
+    //     }
+    // };
 
     //functions for responsive menu
     toggleButton.addEventListener("click", function (event) {
-        event.preventDefault();
-        
-        if(!headerNav.classList.contains("is-open")){
-            let headerContainerHeight = headerContainer.scrollHeight;
-        headerNav.style.setProperty("top", headerContainerHeight + "px");
-        }
-
-        let menu = document.getElementById("main-menu");
-        menu.classList.toggle("is-open");
-
-        if(headerNav.classList.contains("is-open")){
-            headerNav.style.setProperty("height", headerNavHeight + 3 + "px");
-        }else {
-            headerNav.style.setProperty("height", 0);
-        }
+        toggleMenu(headerContainer, headerNav);
+        console.log("click");
     });
+
+
+    function toggleMenu(headerContainer, navMenu) {
+        event.preventDefault();
+
+        if (navMenu.classList.contains("is-open")) {
+            navMenu.style.setProperty("height", 0);
+        }else {
+            let headerContainerHeight = headerContainer.scrollHeight;
+            let navHeight = navMenu.scrollHeight;
+            navMenu.style.setProperty("height", navHeight + "px");
+            navMenu.style.setProperty("top", headerContainerHeight + "px");
+        }
+
+        navMenu.classList.toggle("is-open");
+    }
+
 })();
