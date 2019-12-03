e0226d24414219fe0ded45cedd33ea726558fb7d
andresns <andresm.navarro@gmail.com>
2019-12-03 10:44:52 -0800

    fix header problems when resizing

--- project02/build/js/script.js
+++ project02/build/js/script.js
@@ -3,44 +3,33 @@
     let headerContainer = document.getElementsByClassName("header")[0];
     let headerNav = document.getElementsByClassName("header__nav")[0];
     let menu = document.getElementById("main-menu");
-    // let headerNavHeight = headerNav.scrollHeight;
 
-    // window.onresize = function(event) {
-    //     let headerNavHeight = headerNav.scrollHeight;
-    //     if(window.innerWidth>=1024){
-    //         if(headerNav.classList.contains("is-open")){
-    //             headerNav.style.setProperty("height", "auto");
-    //             headerNav.classList.remove("is-open");
-    //         }
-    //     }else{
-    //         headerNav.classList.remove("is-open");
-    //         headerNav.style.setProperty("height", 0);
-    //         // headerContainerHeight.style.setProperty("height", 0);
-    //         // let headerContainerHeight = headerContainer.scrollHeight;
-    //         // headerNav.style.setProperty("top", headerContainerHeight + "px");
-    //     }
-    // };
 
-    //functions for responsive menu
+
+    window.onresize = function (event) {
+        if (window.innerWidth >= 1024) {
+            headerNav.classList.remove("is-open");
+            resetHeaderStyles();
+        }
+    };
+
     toggleButton.addEventListener("click", function (event) {
-        toggleMenu(headerContainer, headerNav);
-        console.log("click");
-    });
-
-
-    function toggleMenu(headerContainer, navMenu) {
         event.preventDefault();
 
-        if (navMenu.classList.contains("is-open")) {
-            navMenu.style.setProperty("height", 0);
-        }else {
+        if (headerNav.classList.contains("is-open")) {
+            headerNav.style.setProperty("height", 0);
+        } else {
             let headerContainerHeight = headerContainer.scrollHeight;
-            let navHeight = navMenu.scrollHeight;
-            navMenu.style.setProperty("height", navHeight + "px");
-            navMenu.style.setProperty("top", headerContainerHeight + "px");
+            let navHeight = headerNav.scrollHeight;
+            headerNav.style.setProperty("height", navHeight + "px");
+            headerNav.style.setProperty("top", headerContainerHeight + "px");
         }
 
-        navMenu.classList.toggle("is-open");
+        headerNav.classList.toggle("is-open");
+    });
+
+    function resetHeaderStyles() {
+        menu.removeAttribute("style");
     }
 
 })();
